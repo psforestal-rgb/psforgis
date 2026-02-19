87b6a07ee5dfe9261b8bd4c34b0ce5110eff5d42
psforestal-rgb <psforestal@yahoo.com>
2026-02-19 16:17:16 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'gis-cr-cache-v6.5';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  '[https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.css](https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.css)',
+  '[https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.js](https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.js)',
+  '[https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.9.2/proj4.js](https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.9.2/proj4.js)',
+  '[https://cdnjs.cloudflare.com/ajax/libs/jsts/2.9.3/jsts.min.js](https://cdnjs.cloudflare.com/ajax/libs/jsts/2.9.3/jsts.min.js)',
+  '[https://cdnjs.cloudflare.com/ajax/libs/shpjs/4.0.4/shp.min.js](https://cdnjs.cloudflare.com/ajax/libs/shpjs/4.0.4/shp.min.js)',
+  '[https://cdnjs.cloudflare.com/ajax/libs/togeojson/0.16.0/togeojson.min.js](https://cdnjs.cloudflare.com/ajax/libs/togeojson/0.16.0/togeojson.min.js)',
+  '[https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js](https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js)',
+  '[https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js](https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js)',
+  '[https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js](https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js)',
+  '[https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js](https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js)'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || event.request.url.includes('wfs')) return;
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
